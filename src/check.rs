f4327ae7658ff47ab4b32cc9bed8d8da0b3aea27
Pete Bevin <pete@petebevin.com>
2022-02-24 13:51:47 -0500

    Problem 11: largest product in a grid

---
+++ src/check.rs
@@ -0,0 +1,38 @@
+use owo_colors::OwoColorize;
+use std::time::Duration;
+
+
+#[macro_export]
+macro_rules! check {
+    ($fn:expr, $title:expr, $ans:expr) => {
+        let start = Instant::now();
+        let x: i64 = $fn;
+        let duration = start.elapsed();
+        check::show_check_result(stringify!($fn), $title, x, $ans, duration);
+    };
+}
+
+pub fn show_check_result(expr: &str, title: &str, x: i64, ans: i64, time_taken: Duration) {
+    if x == ans {
+        let millis = time_taken.as_millis();
+        let timing = if millis == 0 {
+            String::new()
+        } else {
+            let mut text = format!(" {}ms", millis);
+            if millis < 1000 {
+                text = format!("{}", text.yellow());
+            } else {
+                text = format!("{}", text.red());
+            }
+            text
+        };
+        println!("{} {} {}{}", "🗸".green(), expr, title, timing);
+    } else {
+        println!(
+            "❌{} = {} (should be {})",
+            expr,
+            x.to_string().red(),
+            ans.to_string().green()
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::time::Duration;
+mod check;
+
 use std::time::Instant;
 
 use euler::factors;
@@ -9,40 +10,6 @@ use itertools::Itertools;
 use owo_colors::OwoColorize;
 use primal::Sieve;
 use primal::StreamingSieve;
-
-macro_rules! check {
-    ($fn:expr, $title:expr, $ans:expr) => {
-        let start = Instant::now();
-        let x: i64 = $fn;
-        let duration = start.elapsed();
-        show_check_result(stringify!($fn), $title, x, $ans, duration);
-    };
-}
-
-fn show_check_result(expr: &str, title: &str, x: i64, ans: i64, time_taken: Duration) {
-    if x == ans {
-        let millis = time_taken.as_millis();
-        let timing = if millis == 0 {
-            String::new()
-        } else {
-            let mut text = format!(" {}ms", millis);
-            if millis < 1000 {
-                text = format!("{}", text.yellow());
-            } else {
-                text = format!("{}", text.red());
-            }
-            text
-        };
-        println!("{} {} {}{}", "🗸".green(), expr, title, timing);
-    } else {
-        println!(
-            "❌{} = {} (should be {})",
-            expr,
-            x.to_string().red(),
-            ans.to_string().green()
-        );
-    }
-}
 
 fn main() {
     check!(p1(), "Multiples of 3 or 4", 233168);
@@ -55,6 +22,7 @@ fn main() {
     check!(p8(), "Largest product in a series", 23514624000);
     check!(p9(), "Special pythagorean triplet", 31875000);
     check!(p10(), "Summation of primes", 142913828922);
+    check!(p11(), "Largest product in a grid", 70600674);
 
     println!("{} All good", "🗸".green());
 }
@@ -141,4 +109,49 @@ fn p10() -> i64 {
         .filter(move |&p| p < limit)
         .sum();
     sum.try_into().unwrap()
+}
+
+fn p11() -> i64 {
+    let size = 20;
+    let grid = include_str!("p11.txt")
+        .split_ascii_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect::<Vec<i64>>();
+
+    debug_assert_eq!(grid.len(), size * size);
+
+    let hor = max_4prod(&grid, size, |x, y| (x, y));
+    let ver = max_4prod(&grid, size, |x, y| (y, x));
+    let d1a = max_4prod(&grid, size, |x, y| (x + y, y));
+    let d1b = max_4prod(&grid, size, |x, y| (x, x + y));
+    let d2a = max_4prod(&grid, size, |x, y| (size as isize - 1 - x - y, y));
+    let d2b = max_4prod(&grid, size, |x, y| (size as isize - 1 - x - y, x));
+
+    [hor, ver, d1a, d1b, d2a, d2b].into_iter().max().unwrap()
+}
+
+fn max_4prod<F>(grid: &[i64], size: usize, f: F) -> i64
+where
+    F: Fn(isize, isize) -> (isize, isize),
+{
+    let isize = size as isize;
+
+    (0..)
+        .map(|j| {
+            (0..)
+                .map(|i| f(i, j))
+                .take_while(|(x, y)| (0..isize).contains(x) && (0..isize).contains(y))
+                .map(|(x, y)| {
+                    let x: usize = x.try_into().unwrap();
+                    let y: usize = y.try_into().unwrap();
+                    grid[x + y * size]
+                })
+                .tuple_windows()
+                .map(|(a, b, c, d)| a * b * c * d)
+                .max()
+        })
+        .take_while(|max| max.is_some())
+        .max()
+        .unwrap()
+        .unwrap()
 }
