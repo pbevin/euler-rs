d4371781c17d117b66a4d3d5b619042a65306cf0
Pete Bevin <pete@petebevin.com>
2022-02-24 15:06:45 -0500

    Problem 12 (highly divisible triangular number)

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,7 @@ fn main() {
     check!(p9(), "Special pythagorean triplet", 31875000);
     check!(p10(), "Summation of primes", 142913828922);
     check!(p11(), "Largest product in a grid", 70600674);
+    check!(p12(), "Highly divisible triangular number", 76576500);
 
     println!("{} All good", "🗸".green());
 }
@@ -154,4 +155,39 @@ where
         .max()
         .unwrap()
         .unwrap()
+}
+
+fn p12() -> i64 {
+    // Maps prime numbers to count of factors
+
+    // If n = p1^n1 * p2^n2 * p3^n3 * ...
+    // then the number of divisors is (n1 + 1)(n2 + 1)(n3 + 1)...
+    //
+    // Triangle numbers are easy to calculate:
+    //  T(k) = k(k+1)/2.
+    //
+    // In theory, we could speed this up by using the fact that
+    //  T(k) = k(k+1)/2
+    //       = k(k-1)(k+1)/2(k-1)
+    //       = (k+1)T(k-1)/(k-1)
+    // ...and since we're iterating them in order, we can keep
+    // a record of the prime factors of the last two ks.
+    //
+    // But the record keeping turns out to be harder work than
+    // just factoring each T(k) in turn (90ms vs 35ms on this problem
+    // on my laptop). So we just do it the boring way.
+    let sieve = Sieve::new(10_000); // guessing the limit
+    for k in 10.. {
+        let n = k * (k + 1) / 2;
+        let num_factors: usize = sieve.factor(n)
+            .unwrap()
+            .into_iter()
+            .map(|(_, n)| n + 1)
+            .product();
+
+        if num_factors > 500 {
+            return (k * (k + 1) / 2) as i64;
+        }
+    }
+    unreachable!()
 }
